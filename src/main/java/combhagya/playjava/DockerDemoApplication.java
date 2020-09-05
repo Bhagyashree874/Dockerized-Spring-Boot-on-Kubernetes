b305bdbdad35d15904053444c568953d85671b18
Bhagyashree874 <70800792+Bhagyashree874@users.noreply.github.com>
2020-09-05 14:01:22 +0200

    Initial commit for Docker demo

---
+++ src/main/java/combhagya/playjava/DockerDemoApplication.java
@@ -0,0 +1,15 @@
+package combhagya.playjava;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+
+}
